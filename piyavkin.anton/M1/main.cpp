799be77617ac5c0e2f2b2b2f8d073d1181ecd14b
Piyavva <piyavkin05@bk.ru>
2024-10-13 21:02:03 +0300

    Remake main

--- piyavkin.anton/M1/main.cpp
+++ piyavkin.anton/M1/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <chrono>
+#include <iomanip>
 #include "createpoints.hpp"
+#include "getsquare.hpp"
+
+size_t getPositiveNum(char* str)
+{
+  if (str[0] == '-')
+  {
+    throw std::logic_error("Negative number in command line");
+  }
+  return std::stoull(str);
+}
 
 int main(int argc, char* argv[])
 {
@@ -10,47 +22,56 @@ int main(int argc, char* argv[])
     std::cerr << "The number of command line arguments does not meet the requirements\n";
     return 1;
   }
+  size_t tries = 0;
+  size_t seed = 0;
   try
   {
-    if (argv[1][0] == '-')
-    {
-      throw std::logic_error("The number of tests is not positive");
-    }
-    size_t tries = std::stoull(argv[1]);
-    size_t seed = 0;
+    tries = getPositiveNum(argv[1]);
     if (tries == 0)
     {
-      throw std::logic_error("The number of tests is not positive");
+      throw std::logic_error("The number of tests equil 0");
     }
     if (argc == 3)
     {
-      if (argv[2][0] == '-')
-      {
-        throw std::logic_error("The initialization value of the generator is negative");
-      }
-      seed = std::stoull(argv[2]);
-    }
-    long long r = 0;
-    int countThread = 0;
-    std::minstd_rand gen(seed);
-    while (std::cin >> r >> countThread)
-    {
-      if (r <= 0 || countThread <= 0)
-      {
-        throw std::logic_error("The entered radius or number of threads is not positive");
-      }
-      std::vector< std::pair< double, double > > points;
-      points.reserve(tries);
-      piyavkin::createPoints(gen, points, r, tries);
-      for (auto x: points)
-      {
-        std::cout << x.first << ' ' << x.second << '\n';
-      }
+      seed = getPositiveNum(argv[2]);
     }
   }
   catch (const std::exception& e)
   {
     std::cerr << e.what() << '\n';
     return 2;
+  }
+  long long r = 0;
+  int countThreads = 0;
+  std::minstd_rand gen(seed);
+  while (std::cin >> r >> countThreads)
+  {
+    if (r <= 0 || countThreads <= 0)
+    {
+      std::cerr << "The entered radius or number of threads is not positive\n";
+      return 3;
+    }
+    try
+    {
+      std::vector< std::pair< double, double > > points;
+      points.reserve(tries);
+      piyavkin::createPoints(gen, points, r, tries);
+      auto start = std::chrono::high_resolution_clock::now();
+      double square = piyavkin::getSquare(points, r, countThreads);
+      auto end = std::chrono::high_resolution_clock::now();
+      std::cout << std::fixed << std::setprecision(3);
+      auto mcs = std::chrono::duration_cast< std::chrono::microseconds >(end - start).count();
+      std::cout << static_cast< double >(mcs) / 1000 << ' ' << square << '\n';
+    }
+    catch (const std::exception& e)
+    {
+      std::cerr << e.what() << '\n';
+      return 4;
+    }
+  }
+  if (!std::cin.eof())
+  {
+    std::cerr << "Incorrect radius or number of threads\n";
+    return 3;
   }
 }
