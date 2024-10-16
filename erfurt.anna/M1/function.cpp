91d7371a840dff0f066054fd40ea7fad4c8f1195
Anyanari <a.erfurt02@mail.ru>
2024-10-16 23:13:30 +0300

    Realize square function

--- erfurt.anna/M1/function.cpp
+++ erfurt.anna/M1/function.cpp
@@ -22,3 +22,31 @@ void erfurt::countPointsInCircul_th(size_t seed, size_t numOfTests, double radiu
   }
   *res = countPoints;
 }
+
+size_t erfurt::countPointsInCircul(c_it_t begin, c_it_t end)
+{
+  return std::accumulate(begin, end, 0);
+}
+
+double erfurt::getSquare(size_t seed, size_t numOfTests, double radius, unsigned int countOfThreads)
+{
+  size_t threads = std::min(std::thread::hardware_concurrency() - 1, countOfThreads);
+  std::vector< std::thread > ths;
+  ths.reserve(threads);
+  std::vector< size_t > results(threads, 0);
+  size_t per_th = numOfTests / threads;
+  size_t last_th = per_th + numOfTests % threads;
+  size_t i = 0;
+  for (; i < threads - 1; ++i)
+  {
+    ths.emplace_back(countPointsInCircul_th, seed, per_th, radius, results.begin() + i);
+    seed++;
+  }
+  countPointsInCircul_th(seed, last_th, radius, results.begin() + i);
+  for (auto&& th : ths)
+  {
+    th.join();
+  }
+  size_t countPoints = countPointsInCircul(results.cbegin(), results.cend());
+  return (countPoints * 4 * radius * radius) / numOfTests;
+}
