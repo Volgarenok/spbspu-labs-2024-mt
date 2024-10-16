d81469fbdd4fbb3d745ce08585c306825bdbec8e
C3mR4de <NVadim2005@gmail.com>
2024-10-16 12:40:56 +0300

    Added basic error handling logic

--- novikov.vadim/M1/main.cpp
+++ novikov.vadim/M1/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
-  std::cout << "M1\n";
+  if (argc == 1 || argc > 3)
+  {
+    std::cerr << "Неверное кол-во параметров!\n";
+    return -1;
+  }
+
+  ptrdiff_t input_tries = 0;
+  ptrdiff_t input_seed = 0;
+
+  try
+  {
+    input_tries = std::stol(argv[1]);
+
+    if (argc == 3)
+    {
+      input_seed = std::stol(argv[2]);
+    }
+  }
+  catch (const std::invalid_argument& e)
+  {
+    std::cerr << e.what() << "\n";
+    return -1;
+  }
+
+  if (input_tries < 1 || input_seed < 0)
+  {
+    std::cerr << "Некорректные параметры!\n";
+    return -1;
+  }
+
+  size_t tries = input_tries;
+  size_t seed = input_seed;
+
+  std::cout << tries << " " << seed << "\n";
 }
