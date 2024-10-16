cd86619e4f72444786e95b6a26b409c59badd4ba
Anyanari <a.erfurt02@mail.ru>
2024-10-16 23:19:48 +0300

    Realize streamGuard

---
+++ erfurt.anna/M1/streamGuard.cpp
@@ -0,0 +1,11 @@
+#include "streamGuard.hpp"
+
+erfurt::StreamGuard::StreamGuard(std::basic_ios< char >& s) :
+  s_(s), precision_(s.precision()), flags_(s.flags())
+{}
+
+erfurt::StreamGuard::~StreamGuard()
+{
+  s_.precision(precision_);
+  s_.flags(flags_);
+}
