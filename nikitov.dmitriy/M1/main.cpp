02ae1d33d3f4a979d2175d8ba3caf799acdbfc24
Dmitriy <Dmitry5335@yandex.ru>
2024-10-17 11:58:24 +0300

    Add check of command line arguments and parsing of tries and seed

--- nikitov.dmitriy/M1/main.cpp
+++ nikitov.dmitriy/M1/main.cpp
@@ -1,4 +1,27 @@
-int main()
-{
+#include <iostream>
 
+int main(int argc, char* argv[])
+{
+  int seed = 0;
+  int tries = 0;
+  if (argc == 2)
+  {
+    tries = std::stoi(argv[1]);
+  }
+  else if (argc == 3)
+  {
+    tries = std::stoi(argv[1]);
+    seed = std::stoi(argv[2]);
+  }
+  else
+  {
+    std::cerr << "Error: Wrong command line arguments\n";
+    return 1;
+  }
+
+  if (seed < 0 || tries <= 0)
+  {
+    std::cerr << "Error: Wrong seed or tries\n";
+    return 1;
+  }
 }
