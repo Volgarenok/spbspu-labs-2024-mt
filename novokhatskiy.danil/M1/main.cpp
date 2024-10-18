114d6d8dfd602d59a06592e35e91ff00131ea798
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-10-18 21:12:10 +0300

    Add generatePoints

--- novokhatskiy.danil/M1/main.cpp
+++ novokhatskiy.danil/M1/main.cpp
@@ -1,30 +1,18 @@
-#include "iostream"
-#include "utility"
-#include "random"
-#include "chrono"
-#include "thread"
+#include <iostream>
+#include <utility>
+#include <random>
+#include <chrono>
+#include <algorithm>
+#include <functional>
+#include <iomanip>
+#include <exception>
+#include <thread>
+#include "StreamGuard.hpp"
 
 using Point = std::pair< double, double >;
+using vec_p = std::vector< Point >;
 
-class Clicker
-{
-public:
-  Clicker():
-    start_(std::chrono::high_resolution_clock::now())
-  {}
-  double getTime() const
-  {
-    using std::chrono::high_resolution_clock;
-    using std::chrono::duration_cast;
-    using std::chrono::milliseconds;
-    auto t = high_resolution_clock::now();
-    return duration_cast< milliseconds >(t - start_).count();
-  }
-private:
-  std::chrono::time_point< std::chrono::system_clock > start_;
-};
-
-Point createPoint(double& seed, std::random_device& randomDevice)
+Point createPoint(double seed, std::random_device& randomDevice)
 {
   std::uniform_real_distribution< double > randomDouble(seed, seed + 15.0);
   Point p;
@@ -35,30 +23,43 @@ Point createPoint(double& seed, std::random_device& randomDevice)
   return p;
 }
 
-double getSquare(int& r)
+vec_p generateValues(size_t tries, double seed, std::random_device& randomDevice)
 {
-  constexpr double PI = 3.14159;
-  return PI * r * r;
-  //return 4 * r * r;
+  vec_p res(tries);
+  auto func = std::bind(createPoint, seed, std::ref(randomDevice));
+  std::generate(res.begin(), res.end(), func);
+  return res;
 }
 
-void processMK(int& r, int& threads, size_t& tries, double &seed, std::random_device& randomDevice)
+bool isPointInsideCircle(size_t r, Point& p)
+{
+  return (p.first * p.first) + (p.second * p.second) <= (r * r);
+}
+
+double processMK(size_t r, size_t threads, size_t tries, double seed, std::random_device& randomDevice)
 {
   size_t insidePoints{0};
-  Clicker cl;
-  std::cout << "Real sq - " << getSquare(r) << '\n';
+  if (threads > std::thread::hardware_concurrency() - 1)
+  {
+    threads = static_cast< size_t >(std::thread::hardware_concurrency() - 1);
+  }
+  std::vector< std::thread > ths;
+  ths.reserve(threads - 1);
+  size_t per_th = tries / threads;
   for (size_t i = 0; i < tries; ++i)
   {
     Point p = createPoint(seed, randomDevice);
-    if ((p.first * p.first) + (p.second * p.second) <= (r * r))
+    if (isPointInsideCircle(r, p))
     {
       insidePoints++;
     }
   }
-  double time = cl.getTime();
-  double ratio = static_cast< double >(insidePoints / tries);
-  double sq = ratio * 4 * r * r;
-  std::cout << sq << " " << time;
+  for (auto && th : ths)
+  {
+    th.join();
+  }
+  auto ratio = static_cast< double >(insidePoints / tries);
+  return ratio * 4 * r * r;
 }
 
 int main(int argc, char* argv[])
@@ -70,37 +71,41 @@ int main(int argc, char* argv[])
   }
   size_t tries{};
   double seed{};
-  if (argc == 3)
+  try
   {
-    if (std::stoi(argv[1]) < 0 || std::stod(argv[2]) < 0)
+
+    if (argc == 3)
     {
-      std::cerr << "Parameters can't be negative\n";
-      return 1;
+      if (std::stoi(argv[1]) < 0 || std::stod(argv[2]) < 0)
+      {
+        throw std::invalid_argument("Parameters can't be negative\n");
+      }
+      tries = std::stoull(argv[1]);
+      seed = std::stod(argv[2]);
     }
-    tries = std::stoull(argv[1]);
-    seed = std::stod(argv[2]);
-  }
-  else
-  {
-    tries = std::stoull(argv[1]);
-  }
-  const size_t myThreads = std::thread::hardware_concurrency();
-  std::vector< std::thread > ths;
-  ths.reserve(myThreads);
-  
-  int radius, countThreads{};
-  std::random_device randomDevice;
-  std::cout << tries << '\t' << seed << '\n';
-  while (!std::cin.eof())
-  {
-    std::cout << "Enter data:\n>";
-    std::cin >> radius >> countThreads;
-    if (radius < 0 || countThreads < 0)
+    else
     {
-      std::cerr << "Radius and amount of threads can't be negative\n";
-      continue;
+      tries = std::stoull(argv[1]);
     }
-    processMK(radius, countThreads, tries, seed, randomDevice);
-    std::cout << '\n';
+    size_t radius, countThreads{};
+    std::random_device randomDevice;
+    novokhatskiy::StreamGuard guard(std::cout);
+    std::cout << std::setprecision(3) << std::fixed;
+    while (!std::cin.eof())
+    {
+      std::cin >> radius >> countThreads;
+      if (radius <= 0 || countThreads <= 0)
+      {
+        std::cerr << "Radius and amount of threads can't be negative or zero\n";
+        continue;
+      }
+      processMK(radius, countThreads, tries, seed, randomDevice);
+      std::cout << '\n';
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
   }
 }
