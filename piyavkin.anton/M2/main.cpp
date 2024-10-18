f1785bdb205384aec550e689c9c531444f8532f5
Piyavva <piyavkin05@bk.ru>
2024-10-18 22:28:36 +0300

    Add command processing

--- piyavkin.anton/M2/main.cpp
+++ piyavkin.anton/M2/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <map>
+#include <functional>
 #include "circle.hpp"
+#include "commands.hpp"
 
 int main()
 {
-  Circle c("abc", {3, 4}, 5);
-  std::cout << c.name_ << ' ' << c.center_.x << ' ' << c.center_.y << ' ' << c.radius_;
+  using namespace piyavkin;
+  std::map< std::string, Circle > mp;
+  std::map< std::string, std::function< void(std::istream&) > > cmd;
+  cmd["circle"] = std::bind(inputCircle, std::placeholders::_1, std::ref(mp));
+  std::string name;
+  while (std::cin >> name)
+  {
+    try
+    {
+      cmd.at(name)(std::cin);
+    }
+    catch(const std::exception& e)
+    {
+      std::cerr << e.what() << '\n';
+    }
+  }
+  std::cout << mp["abc"].center_.x << mp["abc"].center_.y << mp["abc"].radius_;
 }
