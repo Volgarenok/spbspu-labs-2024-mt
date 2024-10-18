323dc6fecda30dbd188c748f92982a77ec8b5ab2
Dmitriy <Dmitry5335@yandex.ru>
2024-10-18 15:36:47 +0300

    Add skip for random numbers by id

--- nikitov.dmitriy/M1/calculate_square.cpp
+++ nikitov.dmitriy/M1/calculate_square.cpp
@@ -10,13 +10,14 @@ size_t countPart(size_t radius, size_t seed, size_t tries, size_t id)
 {
   std::default_random_engine randomizer(seed);
   std::uniform_real_distribution< double > distribution(0, radius);
+  for (size_t i = 0; i != id * tries * 2; ++i)
+  {
+    distribution(randomizer);
+  }
+
   size_t count = 0;
   for (size_t i = 0; i != tries; ++i)
   {
-    for (size_t j = 0; j != id * 2; ++j)
-    {
-      distribution(randomizer);
-    }
     double x = distribution(randomizer);
     double y = distribution(randomizer);
     if (std::hypot(x, y) <= radius)
