8a0bbc326fe8a7b0c68b7ad8a01c01525ef426a7
sbabinov <serafimbabinov106@gmail.com>
2024-10-19 00:05:43 +0300

    Add part calculation logic

--- babinov.serafim/M1/circles.cpp
+++ babinov.serafim/M1/circles.cpp
@@ -1,4 +1,24 @@
 #include "circles.hpp"
+#include <cmath>
+#include <random>
+#include "randomizer.hpp"
+
+size_t calculatePoints(size_t seed, size_t tries, size_t skips, size_t radius)
+{
+  size_t inTarget = 0;
+  babinov::Randomizer rand(seed, 0, radius);
+  rand.skip(skips);
+  for (size_t i = 0; i < tries; ++i)
+  {
+    double x = rand.next();
+    double y = rand.next();
+    if (std::hypot(x, y) <= radius)
+    {
+      ++inTarget;
+    }
+  }
+  return inTarget;
+}
 
 std::istream& babinov::operator>>(std::istream& in, CircleData& data)
 {
