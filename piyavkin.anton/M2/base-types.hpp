234b9073680986fa2e1fa13ebc6d4e13b2d26501
Piyavva <piyavkin05@bk.ru>
2024-10-18 23:27:59 +0300

    Add output point and rectangle

--- piyavkin.anton/M2/base-types.hpp
+++ piyavkin.anton/M2/base-types.hpp
@@ -1,5 +1,6 @@
 #ifndef POINT_HPP
 #define POINT_HPP
+#include <ostream>
 
 namespace piyavkin
 {
@@ -8,11 +9,12 @@ namespace piyavkin
     double x;
     double y;
   };
-
+  std::ostream& operator<<(std::ostream& out, const point_t& p);
   struct rectangle_t
   {
     point_t ll;
     point_t ur;
   };
+  std::ostream& operator<<(std::ostream& out, const rectangle_t& r);
 }
 #endif
