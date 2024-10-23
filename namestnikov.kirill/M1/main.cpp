a23ac64ba7f71b95e0dd52559d4c9a89c42ff82a
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-10-23 21:03:42 +0300

    added threads

--- namestnikov.kirill/M1/main.cpp
+++ namestnikov.kirill/M1/main.cpp
@@ -1,7 +1,10 @@
 #include <cmath>
 #include <random>
+#include <thread>
 #include <vector>
 #include <iostream>
+#include <future>
+#include <algorithm>
 
 struct Point{
 public:
@@ -22,14 +25,19 @@ private:
   double y_;
 };
 
-bool checkIfInCircle(const Point & point, double radius)
+bool checkIfInCircle(const Point & point, int radius)
 {
   return (std::pow(point.getX(), 2) + std::pow(point.getY(), 2) <= std::pow(radius, 2)); 
 }
 
-void countPoints(std::default_random_engine & gen, size_t numberOfTests, double radius, size_t & res)
+size_t countPoints(std::default_random_engine & gen, size_t numberOfTests, int radius, size_t skipsCount)
 {
   std::uniform_real_distribution< double > distribution(-radius, radius);
+  for (size_t i = 0; i < skipsCount; ++i)
+  {
+    distribution(gen);
+  }
+  int res = 0;
   for (size_t i = 0; i < numberOfTests; ++i)
   {
     double x = distribution(gen);
@@ -41,16 +49,40 @@ void countPoints(std::default_random_engine & gen, size_t numberOfTests, double 
       ++res;
     }
   }
+  return res;
+}
+
+double getSquare(std::default_random_engine & gen, size_t numberOfTests, int radius, size_t threadsCount)
+{
+  size_t possibleThreads = std::thread::hardware_concurrency();
+  threadsCount = std::min(possibleThreads, threadsCount);
+  std::vector< std::future< size_t > > fts;
+  fts.reserve(threadsCount - 1);
+  std::vector< size_t > results(threadsCount, 0);
+  size_t skipsCount = 0;
+  for (size_t i = 0; i < threadsCount - 1; ++i)
+  {
+    fts.emplace_back(std::async(countPoints, std::ref(gen), numberOfTests / threadsCount, radius, skipsCount));
+    skipsCount += numberOfTests / threadsCount;
+  }
+  results.back() = countPoints(gen, numberOfTests / threadsCount + numberOfTests % threadsCount, radius, skipsCount);
+  std::transform(fts.begin(), fts.end(), results.begin(),
+   [](auto && ft)
+   {
+    return ft.get();
+   });
+   size_t res = std::accumulate(results.begin(), results.end(), 0);
+   return 4 * std::pow(radius, 2) * res / numberOfTests; 
 }
 
 int main()
 {
   size_t seed = 5;
-  double radius = 5.0;
+  double radius = 5;
   size_t numberOfTests = 10000000;
   size_t res = 0;
   std::default_random_engine gen(seed);
-  countPoints(gen, numberOfTests, radius, res);
+  //res = countPoints(gen, numberOfTests, radius);
   std::cout << 4 * std::pow(radius, 2) * res / numberOfTests << "\n";
   std::cout << 3.1415 * radius * radius;
   return 0;
