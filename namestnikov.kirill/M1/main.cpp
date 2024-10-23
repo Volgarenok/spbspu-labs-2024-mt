a927ec7d3986f6d7c3c7ba5cd2cd325f4405fe99
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-10-23 19:18:17 +0300

    calculate square of the circle

--- namestnikov.kirill/M1/main.cpp
+++ namestnikov.kirill/M1/main.cpp
@@ -27,25 +27,31 @@ bool checkIfInCircle(const Point & point, double radius)
   return (std::pow(point.getX(), 2) + std::pow(point.getY(), 2) <= std::pow(radius, 2)); 
 }
 
-int main()
+void countPoints(std::default_random_engine & gen, size_t numberOfTests, double radius, size_t & res)
 {
-  size_t seed = 5;
-  double radius = 5.0;
-  size_t numberOfTests = 5;
-  size_t res = 0;
-  std::default_random_engine gen(seed);
   std::uniform_real_distribution< double > distribution(-radius, radius);
   for (size_t i = 0; i < numberOfTests; ++i)
   {
     double x = distribution(gen);
     double y = distribution(gen);
-    std::cout << x << " " << y;
+    //std::cout << x << " " << y;
     Point p(x, y);
     if (checkIfInCircle(p, radius))
     {
       ++res;
     }
   }
-  std::cout << res;
+}
+
+int main()
+{
+  size_t seed = 5;
+  double radius = 5.0;
+  size_t numberOfTests = 10000000;
+  size_t res = 0;
+  std::default_random_engine gen(seed);
+  countPoints(gen, numberOfTests, radius, res);
+  std::cout << 4 * std::pow(radius, 2) * res / numberOfTests << "\n";
+  std::cout << 3.1415 * radius * radius;
   return 0;
 }
