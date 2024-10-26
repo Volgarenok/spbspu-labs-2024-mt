ad052db1bc4d8c9203f47fa6386627a09f742a33
aRemmie <arina.redko.05@mail.com>
2024-10-26 20:13:06 +0300

    added input processing

--- redko.arina/M1/main.cpp
+++ redko.arina/M1/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <iomanip>
+#include <chrono>
 #include "areaProcessing.hpp"
 
 int main(int argc, char ** argv)
 {
   int tries = 0;
-  size_t seed = 0;
+  int seed = 0;
   try
   {
     if (argc == 2)
@@ -22,7 +24,7 @@ int main(int argc, char ** argv)
       return 1;
     }
   }
-  catch (const std::invalid_argument & e)
+  catch (const std::invalid_argument &)
   {
     std::cerr << "invalid command parameters\n";
     return 1;
@@ -34,17 +36,26 @@ int main(int argc, char ** argv)
     return 2;
   }
 
+  std::cout << std::fixed << std::setprecision(3);
   int numOfThreads = 0;
   int radius = 0;
-  std::cin >> radius >> numOfThreads;
-  if (radius <= 0 || numOfThreads <= 0)
+  while (std::cin >> radius >> numOfThreads)
   {
-    std::cerr << "wrong input parameters\n";
-    return 2;
+    if (radius <= 0 || numOfThreads <= 0)
+    {
+      std::cerr << "wrong input parameters\n";
+      return 2;
+    }
+    auto begin = std::chrono::steady_clock::now();
+    double area = redko::calculateCircleArea(radius, numOfThreads, tries, seed);
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast< std::chrono::milliseconds >(end - begin);
+    std::cout << time.count() << ' ' << area << '\n';
   }
-
-  double circleArea = redko::calculateCircleArea(radius, numOfThreads, tries, seed);
-  std::cout << circleArea << '\n';
-
+  if (!std::cin.eof())
+  {
+    std::cerr << "invalid input\n";
+    return 1;
+  }
   return 0;
 }
