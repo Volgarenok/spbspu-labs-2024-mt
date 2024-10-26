e03620cebab60768e283347a28298d44b59baecd
Rail <ksovfox@mail.ru>
2024-10-26 22:32:02 +0300

    Added input processing

--- zhalilov.rail/M1/main.cpp
+++ zhalilov.rail/M1/main.cpp
@@ -1,7 +1,47 @@
+#include <chrono>
+#include <iomanip>
 #include <iostream>
 
-int main()
+#include "calculateSquare.hpp"
+
+int main(int argc, char* argv[])
 {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+  if (argc != 2 && argc != 3)
+  {
+    std::cerr << "Invalid arguments\n";
+    return 1;
+  }
+
+  size_t iterations = 0;
+  size_t seed = 0;
+  try
+  {
+    iterations = std::stoll(argv[1]);
+    if (argc == 3)
+    {
+      seed = std::stoll(argv[2]);
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+
+  size_t radius = 0;
+  size_t threads = 0;
+  std::cout << std::setprecision(3) << std::fixed;
+  while (std::cin >> radius >> threads)
+  {
+    auto start = std::chrono::high_resolution_clock::now();
+    double square = zhalilov::calcCircleSquare(radius, threads, iterations, seed);
+    auto end = std::chrono::high_resolution_clock::now();
+    double time = std::chrono::duration_cast< std::chrono::milliseconds >(end - start).count();
+    std::cout << time << ' ' << square << '\n';
+  }
+  if (!std::cin)
+  {
+    std::cerr << "Invalid input\n";
+    return 1;
+  }
 }
