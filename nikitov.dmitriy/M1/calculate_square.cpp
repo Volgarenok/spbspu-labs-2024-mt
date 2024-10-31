7a6e8f64ad62f40941b91fb4abeeaa7c705a3fb5
Dmitriy <Dmitry5335@yandex.ru>
2024-10-31 12:17:22 +0300

    Move random nums generation to main thread

--- nikitov.dmitriy/M1/calculate_square.cpp
+++ nikitov.dmitriy/M1/calculate_square.cpp
@@ -6,20 +6,15 @@
 #include <random>
 #include <vector>
 
-size_t countPart(size_t radius, size_t seed, size_t tries, size_t id)
-{
-  std::default_random_engine randomizer(seed);
-  std::uniform_real_distribution< double > distribution(0, radius);
-  for (size_t i = 0; i != id * tries * 2; ++i)
-  {
-    distribution(randomizer);
-  }
+#include <iostream>
 
+size_t countPart(size_t radius, size_t tries, typename std::vector< double >::const_iterator iterator)
+{
   size_t count = 0;
   for (size_t i = 0; i != tries; ++i)
   {
-    double x = distribution(randomizer);
-    double y = distribution(randomizer);
+    double x = *(iterator++);
+    double y = *(iterator++);
     if (std::hypot(x, y) <= radius)
     {
       ++count;
@@ -33,21 +28,31 @@ double nikitov::calculateSquare(size_t radius, size_t numberOfThreads, size_t se
   size_t threads = std::thread::hardware_concurrency();
   numberOfThreads = std::min(numberOfThreads, threads);
 
+  std::vector< double > nums(tries * 2, 0.0);
+
+  std::default_random_engine randomizer(seed);
+  std::uniform_real_distribution< double > distribution(0.0, radius);
+
+  for (size_t i = 0; i != tries * 2; ++i)
+  {
+    nums[i] = distribution(randomizer);
+  }
+
   std::vector< std::future< size_t > > futures;
   futures.reserve(numberOfThreads - 1);
   std::vector< size_t > counts(numberOfThreads, 0);
 
   for (size_t i = 0; i != numberOfThreads - 1; ++i)
   {
-    futures.emplace_back(std::async(countPart, radius, seed, tries / numberOfThreads, i));
+    futures.emplace_back(std::async(countPart, radius, tries / numberOfThreads, nums.cbegin() + (tries * 2 / numberOfThreads * i)));
   }
-  counts.back() = countPart(radius, seed, tries / numberOfThreads + tries % numberOfThreads, numberOfThreads - 1);
+  counts.back() = countPart(radius, tries / numberOfThreads + tries % numberOfThreads, nums.cbegin() + (tries * 2 / numberOfThreads * (numberOfThreads - 1)));
 
   std::transform(futures.begin(), futures.end(), counts.begin(),
-  [](auto && future)
-  {
-    return future.get();
-  });
+    [](auto && future)
+    {
+      return future.get();
+    });
 
   return 4.0 * std::pow(radius, 2) * std::accumulate(counts.cbegin(), counts.cend(), 0) / tries;
 }
