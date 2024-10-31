04cb5d633c7fc19119ba987ee9108ffd21e20ceb
Piyavva <piyavkin05@bk.ru>
2024-11-01 01:35:40 +0300

    Add and fix multiprocessing

--- piyavkin.anton/M2/main.cpp
+++ piyavkin.anton/M2/main.cpp
@@ -8,10 +8,13 @@
 #include <unistd.h>
 #include <functional>
 #include "commands.hpp"
+#include "unlinkfileguard.hpp"
 
 int main()
 {
+  const char* path = "piyavkin.anton/M2/1.socket";
   pid_t child_pid = fork();
+  std::cout << child_pid << '\n';
   if (child_pid < 0)
   {
     std::cerr << "Fork has failed\n";
@@ -20,24 +23,67 @@ int main()
   else if (child_pid == 0)
   {
     int listening{socket(AF_UNIX, SOCK_STREAM, 0)};
-    if (!listening)
+    if (listening < 0)
     {
       std::cerr << "Child process has failed\n";
       return 1;
     }
     struct sockaddr_un addr = {0};
     addr.sun_family = AF_UNIX;
-    const char* path = "/socket";
-    strcpy(addr.sun_path, path);
+    strncpy(addr.sun_path, path, sizeof(addr.sun_path) - 1);
+    piyavkin::UnlinkFileGuard fg(listening, addr.sun_path);
     if (bind(listening, reinterpret_cast< const sockaddr* >(&addr), sizeof(addr)) < 0)
     {
       std::cerr << strerror(errno) << '\n';
       return 1;
     }
-    // UnlinkFileGuard fg{addr.sun_path};
+    if (listen(listening, 1024) < 0)
+    {
+      std::cerr << strerror(errno) << '\n';
+      return 1;
+    }
+    int sock{accept(listening, nullptr, 0)};
+    if (sock < 0)
+    {
+      std::cerr << "Acceptance not completed\n";
+      return 1;
+    }
+    char buffer[1024] = {};
+    int sizeMsg = recv(sock, buffer, 1024, 0); //в последний попробовать MSG_WAITALL(Этот флаг просит подождать, пока не придет полное запрошенное количество данных)
+    if (sizeMsg < 0)
+    {
+      std::cerr << "гг";
+      return 1;
+    }
+    std::cout << buffer << '\n';
   }
   else
   {
+    char buffer[] = "какашка";
+    int sock{socket(AF_UNIX, SOCK_STREAM, 0)};
+    if (!sock)
+    {
+      std::cerr << "Failed to create socket\n";
+      return 2;
+    }
+    struct sockaddr_un addr = {0};
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, path, sizeof(addr.sun_path) - 1);
+    piyavkin::UnlinkFileGuard fg(sock, addr.sun_path);
+    sleep(1);
+    if (connect(sock, reinterpret_cast< sockaddr* >(&addr), sizeof(addr)) < 0)
+    {
+      std::cerr << "Connection failed: " << strerror(errno) << '\n';
+      std::cerr << addr.sun_path << '\n';
+      return 2;
+    }
+    int bytes_sent = send(sock, buffer, strlen(buffer), MSG_NOSIGNAL);
+    if (bytes_sent < 0)
+    {
+      std::cerr << "Error sending data\n";
+      close(sock);
+      return 1;
+    }
     using namespace piyavkin;
     circle_t circles;
     set_t sets;
