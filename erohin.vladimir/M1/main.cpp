2da4e1c0e24a8fecee452aa2df3723441168cb24
WarriorVortex <erohinvova01010111@gmail.com>
2024-11-04 21:18:07 +0300

    Added wrong CLA handling

--- erohin.vladimir/M1/main.cpp
+++ erohin.vladimir/M1/main.cpp
@@ -1,2 +1,34 @@
-int main(){}
+#include <iostream>
 
+int main(int argc, char * argv[])
+{
+  if (argc < 2 || argc > 3)
+  {
+    std::cerr << "Invalid command line arguments number: " <<  argc << "\n";
+    return 1;
+  }
+  size_t tries_number = 0;
+  try
+  {
+    tries_number = std::stoull(argv[1]);
+  }
+  catch (const std::exception & e)
+  {
+    std::cerr << e.what() << "\n";
+    return 2;
+  }
+  size_t seed = 0;
+  if (argc == 3)
+  {
+    try
+    {
+      seed = std::stoull(argv[2]);
+    }
+    catch (const std::exception & e)
+    {
+      std::cerr << e.what() << "\n";
+      return 3;
+    }
+  }
+  return 0;
+}
