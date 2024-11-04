8d11bcdc09129d5d0ea9383122e65896feae3384
WarriorVortex <erohinvova01010111@gmail.com>
2024-11-04 22:09:23 +0300

    Basic logic to calculate circles' square without The Monte Carlo method

---
+++ erohin.vladimir/M1/calculate_circle_square.cpp
@@ -0,0 +1,7 @@
+#include "calculate_circle_square.hpp"
+#include <cmath>
+
+double erohin::calculateCircleSquare(size_t radius, size_t threads_number, size_t tries_number)
+{
+  return M_PI * radius * radius;
+}
---
+++ erohin.vladimir/M1/calculate_circle_square.hpp
@@ -0,0 +1,11 @@
+#ifndef CALCULATE_CIRCLE_SQUARE_HPP
+#define CALCULATE_CIRCLE_SQUARE_HPP
+
+#include <cstddef>
+
+namespace erohin
+{
+  double calculateCircleSquare(size_t radius, size_t threads_number, size_t tries_number);
+}
+
+#endif
