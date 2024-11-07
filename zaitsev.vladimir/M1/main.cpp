fbf1616d3357362cc5aa8df7b49fabd642ef2bb5
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2024-11-08 01:50:47 +0300

    PRNG test

--- zaitsev.vladimir/M1/main.cpp
+++ zaitsev.vladimir/M1/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include "lrnd32.hpp"
-
+#include <random>
 
 int main(int argc, char** argv)
 {
-  lrnd32 gen(41);//41
-
-  for (size_t i = 0; i < 10; ++i)
+  lrnd32 gen(41);
+  size_t groups[64]{};
+  unsigned int x = 67'108'864;
+  std::mt19937 gn(41);
+  for (size_t i = 0; i < 1e7; ++i)
   {
-    std::cout<<gen.generate_next()<<"\n";
+    ++groups[gen.generate_next() / x];
   }
-
+  for (size_t i = 0; i < 64; ++i)
+  {
+    std::cout << i << "---" << groups[i] << '\n';
+  }
   return 0;
 }
