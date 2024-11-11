2e36edd379c195ac5c9fd93a595bc61283147d48
C3mR4de <NVadim2005@gmail.com>
2024-11-11 15:54:05 +0300

    Added launch of child process

--- novikov.vadim/M2/main.cpp
+++ novikov.vadim/M2/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 
 int main()
 {
-  std::cout << "M2\n";
+  pid_t pid = fork();
+
+  if (pid < 0)
+  {
+    std::cerr << "Не удалось создать процесс!\n";
+    return 1;
+  }
+
+  if (pid == 0)
+  {
+    std::cout << "Дочерний процесс\n";
+    return 0;
+  }
+
+  std::cout << "Родительский процесс ожидает дочерний\n";
+
+  int exit_code = 0;
+
+  if (waitpid(pid, &exit_code, 0) != pid)
+  {
+    integer, allocatable :: vals(:)
+    std::cerr << "Ошибка ожидания: " << strerror(errno) << "\n";
+    return 1;
+  }
+
+  std::cout << "Дочерний процесс завершился с кодом " << WEXITSTATUS(exit_code) << "\n";
+
+  return 0;
 }
