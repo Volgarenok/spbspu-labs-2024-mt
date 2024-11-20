1266693331f3d46d361b92ee2d6c38f0daa8b9d5
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2024-11-21 01:37:52 +0300

    Minor fix

--- zaitsev.vladimir/M1/pts_in_circle_counter.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef PTS_IN_CIRCLE_COUNTER
-#define PTS_IN_CIRCLE_COUNTER
-#include <vector>
-#include <random>
-#include "lrnd32.hpp"
-
-void pts_in_circle_counter(size_t radius, size_t iters_nmb, lrnd32 generator, std::vector< size_t >::iterator it);
-#endif
